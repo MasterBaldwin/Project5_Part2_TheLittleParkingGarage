835bea5d3abae9fe0771a0c99efc53be4a501ba7
Mike Baldwin <mikebaldwin21@gmail.com>
2018-02-23 13:35:45 -0500

    Added DecimalFormat to totals.

--- src/ParkingGarage.java
+++ src/ParkingGarage.java
@@ -7,20 +7,22 @@ public static void main(String[] args) {
     double charge;
     double totalHours;
     double totalCharge;
-    DecimalFormat decimalFormat;
+    DecimalFormat hoursFormat;
+    DecimalFormat costFormat;
 
     totalHours = 0;
     totalCharge = 0;
-    decimalFormat = new DecimalFormat("$##.##");
+    hoursFormat = new DecimalFormat("##.#");
+    costFormat = new DecimalFormat("$##.##");
 
     for (int i = 0; i < 5; i++) {
         hours = JOptionPane.showInputDialog("Enter the hours parked for car 1:");
-        charge = 5 + Double.parseDouble(hours) - 2;
+        charge = 5 + Math.ceil(Double.parseDouble(hours)) - 2;
 
         totalHours += Double.parseDouble(hours);
         totalCharge += charge;
     }
 
-    JOptionPane.showMessageDialog(null, "Total Hours " + totalHours + "\n" + "Total Charge " + decimalFormat.format(totalCharge));
+    JOptionPane.showMessageDialog(null, "Total Hours " + hoursFormat.format(totalHours) + "\n" + "Total Charge " + costFormat.format(totalCharge));
 }
 }
