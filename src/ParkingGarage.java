310e95d1e8ec6672dd2111383acfb67d051b3b05
Mike Baldwin <mikebaldwin21@gmail.com>
2018-02-28 15:00:13 -0500

    Reverted back to a workable version.

--- src/ParkingGarage.java
+++ src/ParkingGarage.java
@@ -5,22 +5,20 @@ public class ParkingGarage {
 public static void main(String[] args) {
     double hours, charge, totalHours, totalCost;
     DecimalFormat hoursFormat, costFormat;
-    JPanel uneditablePanel, editablePanel;
+    JPanel panel;
     JTextField hoursText, totalHoursText, totalCostText;
-    //GridLayout layout = new GridLayout(2, 4, 0, 3);
 
     totalHours = 0; totalCost = 0;
-    hoursFormat = new DecimalFormat("##.#"); costFormat = new DecimalFormat("$##.##");
+    hoursFormat = new DecimalFormat("##.#"); costFormat = new DecimalFormat("$##.00");
 
     hoursText = new JTextField(3);
     totalHoursText = new JTextField(3); totalHoursText.setEditable(false);
     totalCostText = new JTextField(3); totalCostText.setEditable(false);
 
-    uneditablePanel = new JPanel();
-    uneditablePanel.setLayout(new BoxLayout(uneditablePanel, BoxLayout.X_AXIS));
-    uneditablePanel.add(new JLabel("Total Hours:"));  uneditablePanel.add(totalHoursText);
-    uneditablePanel.add(new JLabel("Total Cost:"));   uneditablePanel.add(totalCostText);
-    uneditablePanel.add(new JLabel("Hours Parked:")); uneditablePanel.add(hoursText);
+    panel = new JPanel();
+    panel.add(new JLabel("Total Hours:")); panel.add(totalHoursText);
+    panel.add(new JLabel("Total Cost:")); panel.add(totalCostText);
+    panel.add(new JLabel("Hours Parked:")); panel.add(hoursText);
 
     for (int i = 0; i < 5; i++) {
         hoursText.setText("0");
@@ -28,13 +26,7 @@ public static void main(String[] args) {
         totalCostText.setText(Double.toString(totalCost));
         JOptionPane.showMessageDialog(null, panel, "Parking Garage", JOptionPane.QUESTION_MESSAGE);
         hours = Double.parseDouble(hoursText.getText());
-        charge = 5;
-
-        if (hours - 2 > 0) {
-            charge += Math.ceil(hours) - 2;
-        }
-        charge = clamp(charge, 5, 12);
-        //charge = 5 + Math.ceil(hours) - 2;
+        charge = clamp(5 + Math.ceil(hours) - 2, 5, 12);
         totalHours += hours;
         totalCost += charge;
     }
@@ -46,8 +38,7 @@ public static void main(String[] args) {
 /**
  * Clamps a value to a minimum and maximum value (both inclusive).
  */
-public static double clamp(double value, double min, double max)
-{
+public static double clamp(double value, double min, double max) {
     return value < min ? min : value > max ? max : value;
 }
 }
