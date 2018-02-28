f17bf896bcc68e55e32dce36c70759e08f27ce51
Mike Baldwin <mikebaldwin21@gmail.com>
2018-02-27 21:41:47 -0500

    Added a total hours and cost to the panel.

--- src/ParkingGarage.java
+++ src/ParkingGarage.java
@@ -3,26 +3,49 @@ import java.text.DecimalFormat;
 
 public class ParkingGarage {
 public static void main(String[] args) {
-    String hours;
-    double charge;
-    double totalHours;
-    double totalCharge;
-    DecimalFormat hoursFormat;
-    DecimalFormat costFormat;
+    double hours, charge, totalHours, totalCost;
+    DecimalFormat hoursFormat, costFormat;
+    JPanel panel;
+    JTextField hoursText, totalHoursText, totalCostText;
 
-    totalHours = 0;
-    totalCharge = 0;
-    hoursFormat = new DecimalFormat("##.#");
-    costFormat = new DecimalFormat("$##.##");
+    totalHours = 0; totalCost = 0;
+    hoursFormat = new DecimalFormat("##.#"); costFormat = new DecimalFormat("$##.##");
+
+    hoursText = new JTextField(3);
+    totalHoursText = new JTextField(3); totalHoursText.setEditable(false);
+    totalCostText = new JTextField(3); totalCostText.setEditable(false);
+
+    panel = new JPanel();
+    panel.add(new JLabel("Total Hours:")); panel.add(totalHoursText);
+    panel.add(new JLabel("Total Cost:")); panel.add(totalCostText);
+    panel.add(new JLabel("Hours Parked:")); panel.add(hoursText);
 
     for (int i = 0; i < 5; i++) {
-        hours = JOptionPane.showInputDialog("Enter the hours parked for car 1:");
-        charge = 5 + Math.ceil(Double.parseDouble(hours)) - 2;
+        hoursText.setText("0");
+        totalHoursText.setText(Double.toString(totalHours));
+        totalCostText.setText(Double.toString(totalCost));
+        JOptionPane.showMessageDialog(null, panel, "Parking Garage", JOptionPane.QUESTION_MESSAGE);
+        hours = Double.parseDouble(hoursText.getText());
+        charge = 5;
 
-        totalHours += Double.parseDouble(hours);
-        totalCharge += charge;
+        if (hours - 2 > 0) {
+            charge += Math.ceil(hours) - 2;
+        }
+        charge = clamp(charge, 5, 12);
+        //charge = 5 + Math.ceil(hours) - 2;
+        totalHours += hours;
+        totalCost += charge;
     }
 
-    JOptionPane.showMessageDialog(null, "Total Hours " + hoursFormat.format(totalHours) + "\n" + "Total Charge " + costFormat.format(totalCharge));
+    JOptionPane.showMessageDialog(null, "Total Hours " + hoursFormat.format(totalHours) + "\n" + "Total Charge " +
+            costFormat.format(totalCost));
+}
+
+/**
+ * Clamps a value to a minimum and maximum value (both inclusive).
+ */
+public static double clamp(double value, double min, double max)
+{
+    return value < min ? min : value > max ? max : value;
 }
 }
